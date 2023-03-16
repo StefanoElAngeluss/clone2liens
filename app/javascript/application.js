15fff79ca53f309a762100c6ee56361aac40b38a
stefano <stephanedavy@outlook.fr>
2023-03-16 18:21:03 +0100

    mise en place des graphiques dans administration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails'
+import "chartkick/chart.js"
 import '@rails/actiontext'
 import './controllers'
 import "trix"
