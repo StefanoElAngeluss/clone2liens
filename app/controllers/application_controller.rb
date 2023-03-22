9790d15d756d77973550e46e68a130d2d9456313
stefano <stephanedavy@outlook.fr>
2023-03-22 08:37:52 +0100

    amelioration de navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,14 @@ class ApplicationController < ActionController::Base
         end
     end
 
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
     private
+
+    def user_not_authorized
+        flash[:alert] = "Vous n'êtes pas autorisé(e) à afficher cette page!"
+        redirect_to  root_path
+    end
 
     def set_categories
 		@nav_categories = Category.where(display_in_nav: true).order(:nom)
